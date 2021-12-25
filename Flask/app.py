9aff74dea515f379ae8dca2368c37b3d2fdd3259
hDmtP <pritamdhara914@gmail.com>
2021-12-25 10:52:15 +0530

    just testing app.py

--- Flask/app.py
+++ Flask/app.py
@@ -9,16 +9,20 @@ def hello_world():
 @app.route("/ip", methods=["GET"])
 def index():
     ip_address = request.remote_addr
-    return "Requester IP: " + ip_address
+    #print(request.headers['X-Forwarded-For'])
+    #return "Requester IP: " + ip_address
+    return request.access_route[-1]
 
 @app.route("/ip_public", methods=["GET"])
 def public():
+    
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         return (request.environ['REMOTE_ADDR'])
     else:
         return (request.environ['HTTP_X_FORWARDED_FOR'])
-
+    
 if __name__ == "__main__":
-    app.run(debug=True, port=80)
+    app.run(debug=True, port=8089)
     
 #https://www.youtube.com/watch?v=oA8brF3w5XQ&t=840s
+#https://esd.io/blog/flask-apps-heroku-real-ip-spoofing.html
